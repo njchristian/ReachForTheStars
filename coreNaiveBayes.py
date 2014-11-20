dd1bd1cc314bf985534a6f3c6ef2f4e35b3aec77
njchristian <njchristian@neo.tamu.edu>
2014-11-20 10:21:21 -0600

    Core algorithm

--- coreNaiveBayes.py
+++ coreNaiveBayes.py
@@ -1,98 +1,118 @@
 import math
 import re
 
-def pWordIn(word, wordClass, wordCountForClass):
-    wc = wordClass[word]
-    return wc/wordCountForClass
-    
-def initWordCounts:
-    
-    infilename = "collection/"
-    
-    classes = []
-    totals = []
-    
-    #create a different count for each class
-    for i in range(0..5):
-        cs = "class=" + str(i)
-        f=open(infilename + cs,'r')
-        total = 0
+class CoreNaiveBayes(object):
+    @staticmethod
+    def pWordIn(word, wordClass, wordCountForClass):
+        if word in wordClass.keys():
+            wc = wordClass[word]
+            return float(wc)/float(wordCountForClass)
+        else:
+            return 0
         
-        for line in f:
-            arr = re.split('\s+', line)
-            classes[i][arr[0]] = int(arr[1])
-            total = total + int(arr[1])
+    @staticmethod
+    def initWordCounts():
+        
+        infilename = "collection/"
+        
+        classes = []
+        totals = []
+        
+        #create a different count for each class
+        for i in range(0,5):
+            cs = "class=" + str(i+1)
+            f=open(infilename + cs,'r')
+            total = 0
+            classes.append({})
+            for line in f:
+                arr = re.split('\s+', line)
+                classes[i][arr[0]] = int(arr[1])
+                total = total + int(arr[1])
+                
+            totals.append(total)
+                
+        return classes, totals
             
-        totals.append(total)
+    @staticmethod
+    def initWordCountsForUser(uid):
+        
+        infilename = "userProfiles/"+uid+"/"
+        
+        classes = []
+        totals = []
+        
+        #create a different count for each class
+        for i in range(0,5):
+            cs = "class=" + str(i+1)
+            f=open(infilename + cs,'r')
+            total = 0
+            classes.append({})
+            for line in f:
+                arr = re.split('\s+', line)
+                if len(arr) < 2:
+                    continue
+                classes[i][arr[0]] = int(arr[1])
+                total = total + int(arr[1])
+                
+            totals.append(total)
+                
+        return classes, totals
+        
+    @staticmethod
+    def pClassFor(classNum, review, wordClass, wordCountForClass):
+        
+        p = 0
+        
+        wordCounts = []
+        
+        for word in re.findall(r'\w+',review.lower()):
             
-    return classes, totals
-        
-def initWordCountsForUser(uid):
-    
-    infilename = "userProfiles/uid/"
-    
-    classes = []
-    totals = []
-    
-    #create a different count for each class
-    for i in range(0..5):
-        cs = "_class" + str(i)
-        f=open(infilename + cs,'r')
-        total = 0
-        
-        for line in f:
-            arr = re.split('\s+', line)
-            classes[i][arr[0]] = int(arr[1])
-            total = total + int(arr[1])
+            wp = CoreNaiveBayes.pWordIn(word, wordClass, wordCountForClass)
+            #print wp
+            if wp != 0:
+                p = p + math.log(wp)
             
-        totals.append(total)
+        return p
+        
+        
+    #take in a review and reviewer, and return a semantic class!
+    @staticmethod
+    def naiveBayes(review, reviewerId):
+        
+        wordClasses, totalCounts = CoreNaiveBayes.initWordCounts()
+        
+        userWordClasses, userTotalCounts = CoreNaiveBayes.initWordCountsForUser(reviewerId)
+        
+        alpha = .5
+        
+        classProbabilities = []
+        
+        #Go through each class to determine the best fit
+        for i in range(1,6):
+        
+            d = CoreNaiveBayes.pClassFor(i, review, wordClasses[i-1], totalCounts[i-1])
+            u = CoreNaiveBayes.pClassFor(i, review, userWordClasses[i-1], userTotalCounts[i-1])
             
-    return classes, totals
-
-def pClassFor(classNum, review, wordClass, wordCountForClass):
-    
-    p = 1
-    
-    wordCounts = []
-    
-    for word in review:
-        p = p + math.log(pWordIn(word, wordClass, wordCountForClass))
-        
-    return p
-    
-    
-#take in a location id, and return a semantic class!
-def naiveBayes(review, locationId, reviewerId):
-    
-    wordClasses, totalCounts = initWordCounts()
-    
-    userWordClasses, userTotalCounts = initWordCountsForUser(reviewerId)
-    
-    alpha = .5
-    
-    classProbabilities = []
-    
-    #Go through each class to determine the best fit
-    for i in range(1..6):
-    
-        d = pClassFor(i, review, wordClasses[i-1], totalCounts[i-1])
-        u = pClassFor(i, review, userWordClasses[i-1], userTotalCounts[i-1])
-        
-        p = alpha * d + (1-alpha) * u
-        
-        classProbabilites.append(p)
-        
-        
-    maxIndex = 0
-    max = classProbabilities[0]
-    i = 0
-    for c in classProbabilites:
-        if c > max:
-            maxIndex = i
-            max = c
-        i+=1
-        
-    return maxIndex + 1
+            #print d
+            #print u
+            
+            p = alpha * d + (1-alpha) * u
+            
+            classProbabilities.append(p)
+            
+            #print p
+            
+            
+        maxIndex = 0
+        max = classProbabilities[0]
+        i = 0
+        for c in classProbabilities:
+            if c > max:
+                maxIndex = i
+                max = c
+            i+=1
+            
+        return maxIndex + 1
         
         
         
